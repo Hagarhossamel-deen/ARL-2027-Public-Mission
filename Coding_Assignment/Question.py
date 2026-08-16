2f2ec72441482d5631c7663db727e58c38443912
Hagarhossamel-deen <hagarhossam100@gmail.com>
2026-08-17 00:35:58 +0300

    Update Question.py

--- Coding_Assignment/Question.py
+++ Coding_Assignment/Question.py
@@ -1,15 +1,32 @@
 def monitor_following_distance(distances: list[float], speeds: list[float]) -> tuple[int, float, int]:
-    """
-    Analyzes following distance compared to safe distance (speed * 0.5).
+    if not distances:
+        MIN_DIS=0.0
+    else:
+        MIN_DIS=distances[0]
     
-    Args:
-        distances (list[float]): Distance to the lead car at each second.
-        speeds (list[float]): Speed of our car at each second.
+    TAILGATING_SEC=0
+    TAILGATE_INC=0
+    TAILGATE_PREVIOUS_STATE=False
+    TAILGATE_CURRENT_STATE=False
+    SAFE_DIS=0.0
+    for i in range(len(distances)):
+            
+        SAFE_DIS=speeds[i]*0.5
+        if distances[i]<  MIN_DIS:
+            MIN_DIS=distances[i]
         
-    Returns:
-        tuple[int, float, int]: (tailgating_seconds, minimum_distance, tailgate_incidents)
-            - tailgating_seconds: total seconds distance was < safe distance
-            - minimum_distance: absolute closest distance to the lead car (return 0.0 if empty list)
-            - tailgate_incidents: number of separate instances the car started tailgating
-    """
-    pass
+           
+        if  distances[i]<  SAFE_DIS:
+            TAILGATING_SEC=TAILGATING_SEC+1
+            TAILGATE_CURRENT_STATE=True
+        else:
+            TAILGATE_CURRENT_STATE=False
+        if TAILGATE_PREVIOUS_STATE==False and TAILGATE_CURRENT_STATE==True:
+            TAILGATE_INC=TAILGATE_INC+1
+            TAILGATE_PREVIOUS_STATE=TAILGATE_CURRENT_STATE
+            
+        else:
+            TAILGATE_PREVIOUS_STATE=TAILGATE_CURRENT_STATE
+            
+    
+    return (TAILGATING_SEC,MIN_DIS,TAILGATE_INC)
